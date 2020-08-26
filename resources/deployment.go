5ff8b6ed32379659cea665f419b03718076c8854
wxning <wxningopher@gmail.com>
2020-08-26 14:46:44 +0800

    new deployment

---
+++ resources/deployment.go
@@ -0,0 +1,46 @@
+package resources
+
+import (
+	appv1 "github.com/example-inc/memcached-operator/api/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+func NewDeploy(app *appv1.Memcached) *appsv1.Deployment {
+	labels := map[string]string{"app": app.Name}
+	labelSelector := &metav1.LabelSelector{MatchLabels: labels}
+
+	return &appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Deployment",
+			APIVersion: "apps/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      app.Name,
+			Namespace: app.Namespace,
+			OwnerReferences: []metav1.OwnerReference{
+				*metav1.NewControllerRef(app, schema.GroupVersionKind{
+					Group:   v1.SchemeGroupVersion.Group,
+					Version: v1.SchemeGroupVersion.Version,
+					Kind:    "AppService",
+				}),
+			},
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: app.Spec.Size,
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labels,
+				},
+				Spec: corev1.PodSpec{
+					Containers: newContainers(app),
+				},
+			},
+			Selector: labelSelector,
+		},
+	}
+
+}
